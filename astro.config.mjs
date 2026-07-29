27ded8ca61daeee67090dc5d17e2d813c2b25577
tsaigerjarek <jarektsaiger@gmail.com>
2026-07-29 19:49:18 +0300

    PR9: оновити astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
+  site: 'https://tsaigerjarek.github.io',
+  base: '/mdush-dnipro-website',
   trailingSlash: 'always',
 });
